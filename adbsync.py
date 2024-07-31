342975f8ba9b10a977b56c783dec81a33386588a
TheAirBlow <theairblow@gmail.com>
2024-07-31 17:42:52 +0300

    adbsync: major rewrite

--- adbsync.py
+++ adbsync.py
@@ -1,42 +1,70 @@
 #!/usr/bin/env python3
+from tqdm.utils import CallbackIOWrapper
+from typing import BinaryIO
+from pathlib import Path
+from adbutils import adb
+from tqdm import tqdm
 import subprocess
 import argparse
 import os
 
-parser = argparse.ArgumentParser(prog="adbsync",description="Syncs directory with an android device")
-parser.add_argument("target", help="Target folder on remote device (required)")
-parser.add_argument("local", help="List of folders that should be synced (default: all in current directory)", nargs='*', default=os.walk("."))
+def shorten(name):
+    if len(name) < 18:
+        return name
+    return f"{name[:9]}^{name[-8:]}"
+
+parser = argparse.ArgumentParser(prog="adbsync", description="Syncs directory with a remove ADB device")
+parser.add_argument("source", help="Target directory on local device (required)", default=".")
+parser.add_argument("target", help="Target directory on remote device (required)")
+parser.add_argument("-d", "--device", help="Serial identifier of ADB device (default: first connected device)", default=None)
 args = parser.parse_args()
 
-# I know that this sucks
-# Please don't complain about it
-for name in args.local:
-    print('!! Syncing', name)
-    subprocess.check_output([
-        'adb', 'shell', f'mkdir -p {args.target}/{name}'
-    ])
-    remote = subprocess.check_output([
-        'adb', 'shell', f'ls {args.target}/{name}'
-    ]).decode().strip().split('\n')
-    local = os.listdir(name)
-    delete = list()
-    for path in remote:
-        if path in local:
-            continue
-        delete.append(f'\"{args.target}/{name}/{path}\"')
-    print(len(delete), 'files will be deleted')
+d = adb.device(serial=args.device)
+print('ADB device:', d.serial)
 
-    if len(delete) > 0:
-        for arr in [delete[i:i + 250] for i in range(0, len(delete), 250)]:
-            subprocess.call(['adb', 'shell', f'rm {' '.join(arr)}'])
+source = Path(args.source)
+target = Path(args.target)
+print('Source:', source)
+print('Target:', target)
 
-    push = list()
-    for path in local:
-        if path in remote:
-            continue
-        push.append(f'./{name}/{path}')
-    print(len(push), 'files will be pushed')
+d.shell(["mkdir", "-p", args.target])
+remote = d.shell(["find", args.target, "-type", "f"]).split('\n')
+local = source.rglob("*")
 
-    if len(push) > 0:
-        for arr in [push[i:i + 250] for i in range(0, len(push), 250)]:
-            subprocess.call(['adb', 'push'] + arr + [f'{args.target}/{name}/'])
+delete = list()
+for rpath in remote:
+    if not rpath:
+        continue
+    lpath = source / Path(rpath).relative_to(target)
+    if lpath.is_file():
+        continue
+    delete.append(rpath)
+
+if len(delete) > 0:
+    print("Deleting", len(delete), "files...")
+    for arr in tqdm([delete[i:i + 250] for i in range(0, len(delete), 250)], desc="Deleting files", unit="batch", position=0, leave=False):
+        d.shell(['rm'] + arr)
+else:
+    print("No files to delete, skipping...")
+
+push = list()
+for lpath in local:
+    if not lpath:
+        continue
+    rpath = target / lpath.relative_to(source)
+    if str(rpath) in remote:
+        continue
+    push.append((lpath, rpath))
+
+if len(push) > 0:
+    print("Pushing", len(push), "files...")
+    progress = tqdm(unit="B", unit_scale=True, position=1, leave=False)
+    for lpath, rpath in tqdm(push, desc="Transferring files", unit="file", position=0, leave=False):
+        with lpath.open("rb") as f:
+            progress.reset(lpath.stat().st_size)
+            progress.set_description(f"{shorten(lpath.name):<18}")
+            wrapper = CallbackIOWrapper(progress.update, f, "read")
+            d.sync.push(wrapper, str(rpath))
+    progress.close()
+else:
+    print("No files to push, skipping...")
